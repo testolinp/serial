a5a9a7e47b3625f42a01430f463f6305a105f1d4
Patricio Testolin <testolinp@gmail.com>
2017-06-24 12:41:39 -0300

    first commit

---
+++ app/router.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('home', {
+    path: '/'
+  });
+
+  this.route('not-found', {
+    path: '*path'
+  });
+});
+
+export default Router;
