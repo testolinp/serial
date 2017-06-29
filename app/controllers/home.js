40b33281f92e7448f719f389de4d823e014a8101
Patricio Testolin <patricio.testolin@dift.co>
2017-06-29 13:51:08 -0300

    start desktop

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -12,12 +12,17 @@ export default Ember.Controller.extend({
       if(this.get('slide.index') >= this.get('slide.total')) { return; }
 
       Ember.$('.hd').removeClass().addClass('hd');
+      Ember.$('.footer').removeClass().addClass('footer');
+      Ember.$('.dots').removeClass().addClass('dots');
+      Ember.$('.dots__list__item').removeClass('dots__list__item--active');
 
       Ember.$('.slide').eq(this.get('slide.index')).addClass('slide--show-out').removeClass('slide--show-in slide--current');
       this.set('slide.index', this.get('slide.index') + 1);
       Ember.$('.slide').eq(this.get('slide.index')).addClass('slide--show-in slide--current');
 
       Ember.$('.hd').addClass('hd--' + this.get('slide.index'));
+      Ember.$('.footer').addClass('footer--' + this.get('slide.index'));
+      Ember.$('.dots').addClass('dots--' + this.get('slide.index'));
 
       // Ember.$('.home__dots li').removeClass('home__dots__item--active');
       // Ember.$('.home__dots li').eq(this.get('slide.index')).addClass('home__dots__item--active');
@@ -27,12 +32,17 @@ export default Ember.Controller.extend({
       if(this.get('slide.index') <= 0) { return; }
 
       Ember.$('.hd').removeClass().addClass('hd');
+      Ember.$('.footer').removeClass().addClass('footer');
+      Ember.$('.dots').removeClass().addClass('dots');
+      Ember.$('.dots__list__item').removeClass('dots__list__item--active');
 
       Ember.$('.slide').eq(this.get('slide.index')).removeClass('slide--show-in slide--current');
       this.set('slide.index', this.get('slide.index') - 1);
       Ember.$('.slide').eq(this.get('slide.index')).removeClass('slide--show-out').addClass('slide--current');
 
       Ember.$('.hd').addClass('hd--' + this.get('slide.index'));
+      Ember.$('.footer').addClass('footer--' + this.get('slide.index'));
+      Ember.$('.dots').addClass('dots--' + this.get('slide.index'));
       // Ember.$('.home__dots li').removeClass('home__dots__item--active');
       // Ember.$('.home__dots li').eq(this.get('slide.index')).addClass('home__dots__item--active');
     },
